16102f843dd6725c63ead44fa8798328fa6dd1bf
pimovietc <pimovietc@gmail.com>
2018-09-08 00:37:47 +0200

    record-update-wip2
    EasyMine API and CMC API in place, data is fetched and combined and synced.
    
    TO DO: fetch historic rates (will slow down sheet)

--- Tools/record.js
+++ Tools/record.js
@@ -28,18 +28,32 @@ function change_form_record() {
   }
 }
 
+// global variables
+// global variables
+// global variables
+var json_data;	//contains fetched json
+var sheet;		//final text area
+var json_count; //global counter that allows for sync after JSON calls.
+var currencies; // array that contains all currencies, addresses, values etc.
+
+// should merge these in a single object!
+var fiat; // contains chosen type of fiat currency
+var kWh; // contains chosen kWh price
+var PWR; // contains chosen PWR consumption.
+
 //Function to generate the record
 function create_record() {
   //Temporary variables
   cointypes = document.getElementsByName('cointype');
   coins = [];
 
-  //Permanent variables
-  var fiat = document.getElementById('fiat').value;
-  var kWh = document.getElementById('pwrcost').value;
-  var PWR = document.getElementById('pwrcons').value;
-  var currencies = [];
-  var sheet = '0';
+  //variables
+  fiat = document.getElementById('fiat').value;
+  kWh = document.getElementById('pwrcost').value;
+  PWR = document.getElementById('pwrcons').value;
+  currencies = [];
+  sheet = '0';
+  json_count = 0; //reset json_counter each time button is pressed
 
   // Gather selected currencies
   for (var i=0, n=cointypes.length;i<n;i++) {
@@ -117,31 +131,61 @@ function create_record() {
   // ACTUAL SHEET GEN STARTS HERE
 
   // Generate sheet
-  sheet += 'TEST OVERVIEW\n'
+  //sheet += 'TEST OVERVIEW\n'
   for (var i=0, n=currencies.length;i<n;i++) {
     if (i<n-1) {
-      sheet += currencies[i].coin + ' ';
+      //sheet += currencies[i].coin + ' ';
     } else {
-      sheet += currencies[i].coin;
+      //sheet += currencies[i].coin;
     }
   }
-  
-  sheet += '\nfiat ' + fiat + '\ncost ' + kWh + '\npower ' + PWR + '\n\n';
+  //sheet += '\nfiat ' + fiat + '\ncost ' + kWh + '\npower ' + PWR + '\n\n';
 
+  sheet += 'Please wait while we fetch your miner\'s information.';
   //remove '0' from string
   sheet = sheet.substr(1);
   document.getElementById('recordsheet').value = sheet;
 
-  json_easymine(doshit,'https://vertcoin.easymine.online/api/minertxhistory/VgT29UCrpQyJphre4LztiU1qf1cAaA4RNG');
+  //reset sheet's content
+  sheet = '';
+
+  // Probably should fetch fiat stuff first, then do this in callback of that JSON call.
+  var api_url;
+  for (var i=0, n=currencies.length;i<n;i++) {
+    switch(currencies[i].coin) {
+      case 'MON':
+        api_url = 'monacoin';
+        currencies[i].CMCid = 213;
+        break;
+      case 'NIX':
+        api_url = 'nix';
+        currencies[i].CMCid = 2991;
+        break;
+      case 'VTC':
+        api_url = 'vertcoin';
+        currencies[i].CMCid = 99;
+        break;
+      case 'ARG':
+        api_url = 'argentum';
+        currencies[i].CMCid = 31;
+        break;
+      case 'XMY':
+        api_url = 'myriad';
+        currencies[i].CMCid = 182;
+        break;
+      case 'UIS':
+        api_url = 'unitus';
+        currencies[i].CMCid = 781;
+        break;
+    }
+
+    json_easymine(cleanJSON,'https://' + api_url + '.easymine.online/api/minertxhistory/' + currencies[i].address, i);
+  }
   
 }
 
-// global variable
-var json_data;
-
-function doshit() {
+function cleanJSON(count) {
   payoutindice = [];
-  
   // Find the indices where there are payouts in the tx history.
   for (var i=0, n=json_data.tx.length;i<n;i++) {
     if (json_data.tx[i].type == 'payout') {
@@ -170,26 +214,66 @@ function doshit() {
     sum += parseFloat(json_data.tx[i].amount);
   }
   sum = sum*-1;
-  console.log(sum);
+  sum = sum.toFixed(8);
 
-  //contains all payouts
-  console.log(json_data.tx);
+  //add total number of tokens to currencies[]
+  currencies[count].token_sum = sum;
+
+  json_coinmarket(doStuffwithJSON, count);
+}
+
+function doStuffwithJSON(fiat_price, count) {
+	// Add all fiat_price s to currencies.
+	currencies[count].fiat_price = fiat_price;
+
+	//Currently out of order!
+	//Do this in a for-loop after sync to order!
+	sheet += 'You have mined ' + currencies[count].token_sum + ' ' + currencies[count].coin + '. Which is equal to ' + (currencies[count].fiat_price * currencies[count].token_sum) + ' ' + fiat + '.\n';
+
+	// Wait till all crypto JSON calls are finished.
+	json_count++;
+	if (json_count == currencies.length) {
+		// in here is synced!
+		console.log('All EasyMine API calls fetched!');
+		document.getElementById('recordsheet').value = sheet;
+	}
+
+}
+
+function doesNothing() {
+	//nothing
 }
 
 
 // EasyMine JSON call
-function json_easymine(callback, targetUrl) {
+function json_easymine(callback, targetUrl, count) {
   var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   fetch(proxyUrl + targetUrl)
   .then(blob => blob.json())
   .then(data => {
-    console.table(data);
+    //console.table(data);
     json_data = data;
-    callback();
+    callback(count);
     return data;
   })
   .catch(e => {
     console.log(e);
     return e;
   });
+}
+
+// CoinMarketCap JSON call, sends fiat_price to callback()
+function json_coinmarket(callback, count) {
+	fetch('https://api.coinmarketcap.com/v2/ticker/'+currencies[count].CMCid+'/?convert='+fiat)
+  	.then(blob => blob.json())
+  	.then(data => {
+    	//console.table(data);
+    	//console.log(data.data.quotes[fiat].price);
+    	callback(data.data.quotes[fiat].price, count);
+    	return data;
+  	})
+  	.catch(e => {
+    	console.log(e);
+    	return e;
+  	});
 }
